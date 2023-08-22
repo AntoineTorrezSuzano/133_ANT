bf3afda771a810b2d06acf6dff1b2a9b4ff31df4
Antoine Torrez Suzano <antoine.torrezsuzano@ceff.ch>
2023-08-22 11:22:53 +0200

    Ajouté exercice 3
    et mise a niveau de l'exercice 2

---
+++ exercice_2.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exercice_2</title>
+</head>
+<body>
+<?php
+echo "\n<table border=\"2\">";
+echo "\n\t<tr>";
+echo "\n\t\t<td>Nom</td>";
+echo "\n\t\t<td>Prénom</td>";
+echo "\n\t</tr>";
+echo "\n\t<tr>";
+echo "\n\t\t<td>Lerdorf</td>";
+echo "\n\t\t<td>Rasmus</td>";
+echo "\n\t</tr>";
+echo "\n\t<tr>";
+echo "\n\t\t<td>Computer</td>";
+echo "\n\t\t<td>Jules</td>";
+echo "\n\t</tr>";
+echo "\n</table>";
+echo "\n";
+
+?>
+</body>
+</html>
---
+++ exercice_3.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exercice_3</title>
+</head>
+<body>
+    <?php
+        $nom = "Torrez Suzano";
+        $prenom = "Antoine";
+        $profession = "Developeur";
+
+        echo "<b>$nom, $prenom</b>, $profession<br />"
+
+    ?>
+</body>
+</html>
